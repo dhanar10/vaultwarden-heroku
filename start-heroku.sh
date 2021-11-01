ebb6540f2b1b0d509a26bac250520eb084c12c6a
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2021-11-01 07:58:46 +0700

    fix: persistent rsa key (more)

--- start-heroku.sh
+++ start-heroku.sh
@@ -6,7 +6,7 @@ export ROCKET_PORT="${PORT}"
 # Use persistent RSA key stored in Heroku config
 if [ -n "${RSA_KEY_TGZ}" ]; then
   export RSA_KEY_FILENAME=data/rsa_key
-  echo "${RSA_KEY_TGZ}" | base64 -d | tar zxvf - -C data
+  echo "${RSA_KEY_TGZ}" | base64 -d | tar zxf - -C data
 fi
 
 /bin/sh /start.sh
